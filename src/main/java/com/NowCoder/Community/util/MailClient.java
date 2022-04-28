b45cf7d3684557d40d3c3f08eddeda608cac4183
liuxingyu <541905573@qq.com>
2022-04-28 10:42:44 +0800

    Mail测试

---
+++ src/main/java/com/NowCoder/Community/util/MailClient.java
@@ -0,0 +1,41 @@
+package com.NowCoder.Community.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Component
+public class MailClient {
+
+    private static final Logger logger= LoggerFactory.getLogger((MailClient.class));
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    //每次发送邮件都是从同一个用户发出，所以在这里直接将username注入，省的一遍一遍读取
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendMail(String to,String subject,String content)
+    {
+        try {
+            MimeMessage message=mailSender.createMimeMessage();
+            MimeMessageHelper helper=new MimeMessageHelper(message);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(content,true);//如果不加true会以文本发送，加了以html文件发送
+            mailSender.send(helper.getMimeMessage());
+        } catch (MessagingException e) {
+            logger.error("发送邮件失败"+e.getMessage());
+        }
+    }
+
+}
---
+++ src/test/java/com/NowCoder/Community/MailTests.java
@@ -0,0 +1,65 @@
+package com.NowCoder.Community;
+
+import com.NowCoder.Community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Random;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testHtmlMail()
+    {
+        Context context=new Context();
+        context.setVariable("username","sunday");
+        String content= templateEngine.process("/mail/demo",context);
+        System.out.println(content);
+
+        mailClient.sendMail("541905573@qq.com","htmlTest",content);
+    }
+    @Test
+    public void testForgetPwdMail()
+    {
+        Context context=new Context();
+        Random rand=new Random();
+        int p=0;
+        while (p<10000) {
+            p=rand.nextInt(99999);
+        }
+        StringBuffer stringBuffer=new StringBuffer();
+        while (p!=0)
+        {
+            int temp=p%10;
+            p/=10;
+            stringBuffer.append(temp);
+        }
+        String str=stringBuffer.toString();
+        context.setVariable("code",str);
+        String content=templateEngine.process("/mail/forget",context);
+        mailClient.sendMail("2282701633@qq.com","找回密码",content);
+    }
+    @Test
+    public void testTextMail()
+    {
+        mailClient.sendMail("541905573@qq.com","Test","这是一封测试邮件");
+    }
+
+
+
+}
